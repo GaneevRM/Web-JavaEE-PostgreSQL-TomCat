7a199e315885bbedf77f2ca9ea5a4fa35da7f114
GaneevRM <ganeevrm@yahoo.com>
2021-05-05 23:08:12 +0400

    Added Logger to LandlordData.java

--- src/main/java/com/example/WebGaneevRM/data/LandlordData.java
+++ src/main/java/com/example/WebGaneevRM/data/LandlordData.java
@@ -5,6 +5,9 @@ import com.example.WebGaneevRM.tools.DataBaseConnection;
 
 import java.sql.*;
 import java.util.LinkedList;
+import java.util.logging.Level;
+
+import static com.example.WebGaneevRM.data.RenterData.logger;
 
 public class LandlordData {
     public static LinkedList<LandlordDTO> selectLandlord() {
@@ -20,29 +23,30 @@ public class LandlordData {
                 landlordDTO.setEmail(resultSet.getString(4));
                 listLandlord.add(landlordDTO);
             }
-        }catch (SQLException e) {
-            e.printStackTrace();
+        } catch (SQLException e) {
+            logger.log(Level.WARNING,"Ошибка SQL при считывании данных из таблицы landlord", e);
+        } catch (Exception exception) {
+            logger.log(Level.WARNING,"Ошибка при считывании данных из таблицы landlord", exception);
         }
         return listLandlord;
     }
 
-    public static void addLandlord(String nameLandlord, String phoneLandlord, String emailLandlord){
+    public static void addLandlord(String nameLandlord, String phoneLandlord, String emailLandlord) throws SQLException{
         try (Connection connection = DataBaseConnection.getConnection()) {
             PreparedStatement preparedStatement = connection.prepareStatement("INSERT INTO landlord VALUES (nextval('seq_id_landlord'), ?, ?, ?);");
             preparedStatement.setString(1, nameLandlord);
             preparedStatement.setString(2, phoneLandlord);
             preparedStatement.setString(3, emailLandlord);
             preparedStatement.executeUpdate();
-        }catch (SQLException e){
-            e.printStackTrace();
         }
     }
 
     public static void deleteLandlord(int idLandlord) throws SQLException {
-        Connection connection = DataBaseConnection.getConnection();
-        PreparedStatement preparedStatement = connection.prepareStatement("DELETE FROM landlord WHERE id_landlord = ?;");
-        preparedStatement.setInt(1, idLandlord);
-        preparedStatement.executeUpdate();
+        try (Connection connection = DataBaseConnection.getConnection()) {
+            PreparedStatement preparedStatement = connection.prepareStatement("DELETE FROM landlord WHERE id_landlord = ?;");
+            preparedStatement.setInt(1, idLandlord);
+            preparedStatement.executeUpdate();
+        }
     }
 
     public static void editLandlord(int idLandlord, String nameLandlord, String phoneLandlord, String emailLandlord) throws SQLException{
@@ -53,8 +57,6 @@ public class LandlordData {
             preparedStatement.setString(3, emailLandlord);
             preparedStatement.setInt(4, idLandlord);
             preparedStatement.executeUpdate();
-        }catch (SQLException e){
-            e.printStackTrace();
         }
     }
 }
